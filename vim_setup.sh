9912dabb8b77c99c47dd3d029621838421979145
albertgoncalves <goncalves.albert@gmail.com>
2018-09-08 18:36:51 -0400

    Adjusted hotkeys.

--- vim_setup.sh
+++ vim_setup.sh
@@ -13,9 +13,11 @@ echo "set colorcolumn=80"                              >> .vimrc
 echo "set number"                                      >> .vimrc
 echo "colorscheme gummybears"                          >> .vimrc
 echo "highlight ColorColumn ctermbg=0 guibg=lightgrey" >> .vimrc
-echo "xnoremap <leader>p xi()<Esc>P"                   >> .vimrc
+echo "xnoremap <leader>b xi()<Esc>P"                   >> .vimrc
 echo "xnoremap <leader>c xi{}<Esc>P"                   >> .vimrc
 echo "xnoremap <leader>s xi[]<Esc>P"                   >> .vimrc
 echo "xnoremap <leader>q xi""<Esc>P"                   >> .vimrc
 echo "autocmd BufWritePre * %s/\s\+$//e"               >> .vimrc
 echo "set tabstop=4 shiftwidth=4 expandtab"            >> .vimrc
+echo "vmap <leader>y :w! /tmp/vitmp<CR>"               >> .vimrc
+echo "nmap <leader>p :r! cat /tmp/vitmp<CR>"           >> .vimrc
