48bcba88d5600aee427439e1de1ba405996a1ae7
albertgoncalves <goncalves.albert@gmail.com>
2018-09-22 11:26:46 -0400

    Setup for TypeScript syntax highlighting.

--- mac_vim_setup.sh
+++ mac_vim_setup.sh
@@ -15,16 +15,21 @@ curl "https://raw.githubusercontent.com/albertgoncalves/haskell.vim/master/synta
 mkdir .vim/colors
 curl "https://raw.githubusercontent.com/albertgoncalves/Gummybears/master/colors/gummybears.vim" > .vim/colors/gummybears.vim
 
-echo "syntax on"                              > .vimrc
-echo "set colorcolumn=80"                    >> .vimrc
-echo "set number"                            >> .vimrc
-echo "colorscheme gummybears"                >> .vimrc
-echo "highlight ColorColumn ctermbg=7"       >> .vimrc
-echo "xnoremap <leader>b xi()<Esc>P"         >> .vimrc
-echo "xnoremap <leader>c xi{}<Esc>P"         >> .vimrc
-echo "xnoremap <leader>s xi[]<Esc>P"         >> .vimrc
-echo "xnoremap <leader>q xi""<Esc>P"         >> .vimrc
-echo "autocmd BufWritePre * %s/\s\+$//e"     >> .vimrc
-echo "set tabstop=4 shiftwidth=4 expandtab"  >> .vimrc
-echo "vmap <leader>y :w! /tmp/vitmp<CR>"     >> .vimrc
-echo "nmap <leader>p :r! cat /tmp/vitmp<CR>" >> .vimrc
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/tmp/typescript-vim
+cp ~/.vim/tmp/typescript-vim/syntax/typescript.vim ~/.vim/syntax/
+yes | rm -R ~/.vim/tmp/typescript-vim
+
+echo "syntax on"                                                     > .vimrc
+echo "set colorcolumn=80"                                           >> .vimrc
+echo "set number"                                                   >> .vimrc
+echo "colorscheme gummybears"                                       >> .vimrc
+echo "highlight ColorColumn ctermbg=7"                              >> .vimrc
+echo "xnoremap <leader>b xi()<Esc>P"                                >> .vimrc
+echo "xnoremap <leader>c xi{}<Esc>P"                                >> .vimrc
+echo "xnoremap <leader>s xi[]<Esc>P"                                >> .vimrc
+echo "xnoremap <leader>q xi""<Esc>P"                                >> .vimrc
+echo "autocmd BufWritePre * %s/\s\+$//e"                            >> .vimrc
+echo "set tabstop=4 shiftwidth=4 expandtab"                         >> .vimrc
+echo "vmap <leader>y :w! /tmp/vitmp<CR>"                            >> .vimrc
+echo "nmap <leader>p :r! cat /tmp/vitmp<CR>"                        >> .vimrc
+echo "autocmd BufNewFile,BufRead *.ts setlocal filetype=typescript" >> .vimrc
