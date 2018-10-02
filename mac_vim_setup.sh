58293741154357d5a09587d952385bed3b30f2b0
albertgoncalves <goncalves.albert@gmail.com>
2018-10-01 23:01:23 -0400

    This works.

--- mac_vim_setup.sh
+++ mac_vim_setup.sh
@@ -5,6 +5,7 @@ cd ~
 # echo "export PS1=\"\t \w $ \""                >> .bash_profile
 # echo "export PS1=\"\n\t \w \n$ \""            >> .bash_profile
 # echo "export PS1=\"\n\e[0;36m\w\e[m\n$ \""    >> .bash_profile
+# echo "export PS1=\"\e[0;36m\w\e[m $ \""       >> .bach_profile
 # echo "export CLICOLOR=1"                      >> .bash_profile
 # echo "export LSCOLORS=ExFxBxDxCxegedabagacad" >> .bash_profile
 # echo "alias ls='ls -GFh'"                     >> .bash_profile
