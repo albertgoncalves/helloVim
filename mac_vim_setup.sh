1c220e0d258552721543bb696e8431dbb334b033
albertgoncalves <goncalves.albert@gmail.com>
2018-10-01 21:24:14 -0400

    New PS1.

--- mac_vim_setup.sh
+++ mac_vim_setup.sh
@@ -3,6 +3,8 @@
 cd ~
 
 # echo "export PS1=\"\t \w $ \""                >> .bash_profile
+# echo "export PS1=\"\n\t \w \n$ \""            >> .bash_profile
+# echo "export PS1=\"\n\e[0;36m\w\e[m\n$ \""    >> .bash_profile
 # echo "export CLICOLOR=1"                      >> .bash_profile
 # echo "export LSCOLORS=ExFxBxDxCxegedabagacad" >> .bash_profile
 # echo "alias ls='ls -GFh'"                     >> .bash_profile
