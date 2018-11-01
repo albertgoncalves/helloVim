fd387f048e7ee9d2267d04eb78b8954335fb8932
albertgoncalves <goncalves.albert@gmail.com>
2018-11-01 01:15:50 -0400

    Slightly improved.

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ git config --global core.excludesfile ~/.gitignore_global
 
 cd ~
 
-# echo "export PS1=\"-> \w $ \""                >> .bash_profile
-# echo "export CLICOLOR=1"                      >> .bash_profile
-# echo "export LSCOLORS=ExFxBxDxCxegedabagacad" >> .bash_profile
-# echo "alias ls='ls -GFh'"                     >> .bash_profile
+cat << 'EOF' >> .bash_profile
+export PS1="-> \w $ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias ls='ls -GFh'
+EOF
 
 mkdir -p .vim/syntax
 curl "https://raw.githubusercontent.com/albertgoncalves/haskell.vim/master/syntax/haskell.vim" > \
@@ -22,19 +24,21 @@ mkdir .vim/colors
 curl "https://raw.githubusercontent.com/albertgoncalves/gruvbox/master/colors/gruvbox.vim" > \
     .vim/colors/gruvbox.vim
 
-echo "syntax on"                                                     > .vimrc
-echo "set colorcolumn=80"                                           >> .vimrc
-echo "set number"                                                   >> .vimrc
-echo "set background=dark"                                          >> .vimrc
-echo "colorscheme gruvbox"                                          >> .vimrc
-echo "xnoremap <leader>b xi()<Esc>P"                                >> .vimrc
-echo "xnoremap <leader>c xi{}<Esc>P"                                >> .vimrc
-echo "xnoremap <leader>s xi[]<Esc>P"                                >> .vimrc
-echo "xnoremap <leader>q xi""<Esc>P"                                >> .vimrc
-echo "autocmd BufWritePre * %s/\s\+$//e"                            >> .vimrc
-echo "set tabstop=4 shiftwidth=4 expandtab"                         >> .vimrc
-echo "vmap <leader>y :w! /tmp/vitmp<CR>"                            >> .vimrc
-echo "nmap <leader>p :r! cat /tmp/vitmp<CR>"                        >> .vimrc
-echo "autocmd BufNewFile,BufRead *.ts setlocal filetype=typescript" >> .vimrc
-echo "autocmd BufNewFile,BufRead *.stan setlocal filetype=stan"     >> .vimrc
-echo "set cursorline"                                               >> .vimrc
+cat << 'EOF' >> .vimrc
+syntax on
+set colorcolumn=80
+set number
+set background=dark
+colorscheme gruvbox
+xnoremap <leader>b xi()<Esc>P
+xnoremap <leader>c xi{}<Esc>P
+xnoremap <leader>s xi[]<Esc>P
+xnoremap <leader>q xi""<Esc>P
+autocmd BufWritePre * %s/\s\+$//e
+set tabstop=4 shiftwidth=4 expandtab
+vmap <leader>y :w! /tmp/vitmp<CR>
+nmap <leader>p :r! cat /tmp/vitmp<CR>
+autocmd BufNewFile,BufRead *.ts setlocal filetype=typescript
+autocmd BufNewFile,BufRead *.stan setlocal filetype=stan
+set cursorline
+EOF
